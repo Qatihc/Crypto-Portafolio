4cdbeb76bb81d63203755af649571031c21a2874
Matias <matias_casali@hotmail.com>
2022-01-21 19:00:47 -0300

    Agregados logos para las monedas, cambios en los jobs de coin gecko.

--- backend/services/initializeCoinGeckoFetchJobs.js
+++ backend/services/initializeCoinGeckoFetchJobs.js
@@ -2,41 +2,55 @@ const {fetchSupportedCoinsList, fetchCoinPrices}  = require('./CoinGeckoApiReque
 const schedule = require('node-schedule');
 
 
-const initializeCoinGeckoFetchJobs = async (app) => {
+const initializeCoinGeckoFetchJobs = () => {
+  let coinPrices = {};
+  let supportedCoins = {};
+  let symbolToCoinGeckoId = {};
 
   const updateSupportedCoins = async () => {
     try {
-      const supportedCoins = await fetchSupportedCoinsList();
-      // console.log(supportedCoins)
+      supportedCoins = await fetchSupportedCoinsList();
       /* Creo esta estructura para poder encontra la id en O(1) */
-      const symbolToCoinGeckoId = {}
       for (coin of supportedCoins) {
         symbolToCoinGeckoId[coin.symbol.toUpperCase()] = coin.id
       }
-      app.locals = {...app.locals, symbolToCoinGeckoId, supportedCoins};
+      console.log(supportedCoins.length);
     } catch (err) {
       console.log(err)
     }
   }
 
+  const getCoinPriceFromSymbol = (symbol) => {
+    const cgId = symbolToCoinGeckoId[symbol.toUpperCase()]
+    const price = coinPrices[cgId];
+    if (!price) return null;
+    return coinPrices[cgId].usd;
+  }
+
+  const isSupportedCoin = (symbol) => {
+    /* Si no tiene id no esta soportada */
+    return !!symbolToCoinGeckoId[symbol.toUpperCase()];
+  }
+
   const updateCoinPrices = async () => {
     // Por el momento no es necesario limitar las monedas a las que les busco el precio, pero podria llegar a serlo.
-    // const activeCoins = await Coin.distinct('symbol');
-    // const activeCoinsIds = activeCoins.map(c => app.locals.symbolToCoinGeckoId[c.toUpperCase()])
     try {
-      const supportedCoinIds = app.locals.supportedCoins.map(c => c.id)
-      const coinPrices = await fetchCoinPrices(supportedCoinIds);
-      app.locals.coinPrices = coinPrices;
+      const supportedCoinIds = supportedCoins.map(c => c.id);
+      coinPrices = await fetchCoinPrices(supportedCoinIds);
     } catch (err) {
       console.log(err)
     }
   };
 
-  await updateSupportedCoins()
+  (async () => {
+    await updateSupportedCoins();
+    await updateCoinPrices();
+  })()
+
   // const jobUpdateSupportedCoins = schedule.scheduleJob('* * */1 * *', updateSupportedCoins);
-  
-  await updateCoinPrices();
   // const jobUpdateCoinPrices = schedule.scheduleJob('* * */5 * * *', updateCoinPrices);
+
+  return { getCoinPriceFromSymbol, isSupportedCoin }
 }
 
 module.exports = initializeCoinGeckoFetchJobs
